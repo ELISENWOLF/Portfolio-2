1df36f3521d1c15117462e8dc50943270110c537
ELISENWOLF <aravindprakash2001@gmail.com>
2023-04-13 19:16:00 +0530

    navbar-added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import { BrowserRouter } from 'react-router-dom'
-
-import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="realtive z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works/>
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <NavBar />
+    </div>
   );
 }
 
